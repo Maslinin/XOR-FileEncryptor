c5c30a806d6d98cc1fcefe5702a228a4b08f61bb
Danil Rudin <ddanilr@yandex.ru>
2022-04-23 13:13:38 +0300

    older folders was removed

--- src/Encoder/xorencoder.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ENCODER_H
-#define _ENCODER_H
-
-//Encryption Interface:
-int file_encode(const char *srcFilePath, const char *trgtFilePath, const char *key, int encryptMode, int srcFileDelFlag);
-char *generate_key(char *str, int size);
-
-#endif // _ENCODER_H
